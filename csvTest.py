bf28cf5486fc18990b83d520eb9f03c436fc9d3b
MySecondLanguage <mmamunism@gmail.com>
2019-06-12 07:43:24 +0600

    rewrite

--- csvTest.py
+++ csvTest.py
@@ -6,9 +6,11 @@ start_date = dt.datetime(2019, 4,12)
 end_date = dt.datetime(2019, 4,21)
 daterange = pd.date_range(start_date, end_date)
 
+
 XLIST = ['401', 'Pabolo Picasso', '402', 'Justin TED', '303', 'Ariana Great']
 YLIST = ['201', 'Donald Ted', '202', 'Einastain', '203', 'Hillary Cloud']
 ZLIST = ['301', 'Theresa March', '302', 'Jullian Ass Bad', '303', 'Messi Bro']
+
 
 i = 0
 for date in daterange:
